7dd01c18549b029f318851a7069a249c1f31ca26
Rebecca G. <rebeccagoure2@gmail.com>
2026-03-04 12:13:22 -0600

    scrapimg funtions

--- learn-webscraping/houston_gas/function_scraping.R
+++ learn-webscraping/houston_gas/function_scraping.R
@@ -18,37 +18,39 @@ scrape_gas_station_url <- function(city_url){
 # function to scrape the city urls
 
 county_url <- "https://www.gasbuddy.com/gasprices/alabama/autauga/3098"
+scrape_city_url(county_url)
 scrape_city_url <- function(county_url){
   
   city <- read_html_live(county_url) |>
     html_elements( css = ".DataGrid-module__link___1Pa9Z")|>
     html_attr( name = "href")
   
-  html_cty <- glue("https://www.gasbuddy.com{city}")
+  glue("https://www.gasbuddy.com{city}")
 }
 
 # function to scrape the county urls
 
 state_url <- "https://www.gasbuddy.com/gasprices/alabama"
 
+scrape_county_url(state_url )
 scrape_county_url <- function(state_url){
  counties <- read_html_live(state_url) |> 
     html_elements( css = ".AreaCountyList-module__grid___6Le8s+ .AreaCountyList-module__grid___6Le8s .DataGrid-module__link___1Pa9Z")|>
     html_attr( name = "href")
   
-  html_county <- glue("https://www.gasbuddy.com{counties}")
+  glue("https://www.gasbuddy.com{counties}")
   
 }
 
 # founction to scrape the state urls
 
 mainh <- "https://www.gasbuddy.com/gasprices"
-
+scrape_state_url (mainh)
 scrape_state_url <- function(mainh){
   state_province <- read_html_live(mainh) |>
     html_elements( css = ".DataGrid-module__link___1Pa9Z")|>
     html_attr(name = "href")
   
-  html_state_province <- glue("https://www.gasbuddy.com{state_province}")
+   glue("https://www.gasbuddy.com{state_province}")
   
 }
