d3aa043d7ebeff94fb1c4a46effc47c1dfe0b450
G. Nono Gueye <nonoghislain@gmail.com>
2026-03-04 13:31:28 -0600

    refactored scraping functions and created function to scrape all station urls

--- learn-webscraping/houston_gas/scraping_functions.R
+++ learn-webscraping/houston_gas/scraping_functions.R
@@ -49,3 +49,61 @@ scrape_gas_station_data <- function(
       values_to = "price"
     )
 }
+
+
+# Scrape "area" URLs ----
+
+generate_css_classes <- function() {
+  css_class <- c(
+    ".DataGrid-module__link___1Pa9Z",
+    ".AreaCountyList-module__grid___6Le8s+ .AreaCountyList-module__grid___6Le8s .DataGrid-module__link___1Pa9Z",
+    ".AreaCountyList-module__areaItem___3c4w7 .DataGrid-module__link___1Pa9Z",
+    ".StationDisplay-module__stationNameHeader___1A2q8 a"
+  )
+  names(css_class) <- c("state", "county", "city", "station")
+  css_class
+}
+
+
+scrape_urls <- function(
+  url,
+  area_type,
+  user_agent = httr::user_agent(
+    agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+  ),
+  css_class_vector = generate_css_classes()
+) {
+  css_class <- css_class_vector[area_type]
+
+  response <- httr::GET(
+    url = url,
+    user_agent
+  )
+
+  endpoints <- rvest::read_html(x = response) |>
+    html_elements(css = css_class) |>
+    html_attr(name = "href")
+
+  glue("https://www.gasbuddy.com{endpoints}")
+}
+
+
+# Get state
+scrape_all_station_urls <- function(main_url) {
+  state_urls <- scrape_urls(url = main_url, area_type = "state")
+
+  county_urls <- purrr::map(.x = state_urls[1:2], .f = \(state_url) {
+    scrape_urls(url = state_url, area_type = "county")
+  }) |>
+    purrr::flatten_chr()
+
+  city_urls <- purrr::map(.x = county_urls[1:2], .f = \(county_url) {
+    scrape_urls(url = county_url, area_type = "city")
+  }) |>
+    purrr::flatten_chr()
+
+  station_urls <- purrr::map(.x = city_urls[1:2], .f = \(city_url) {
+    scrape_urls(url = city_url, area_type = "station")
+  }) |>
+    purrr::flatten_chr()
+}
